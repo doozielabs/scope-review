2bf93cf5442e0ecc561d667d007c371ceb09c8b7
SyedMehtabali <syedmehtab2125@gmail.com>
2023-04-06 11:06:34 +0500

    fonts issue try 001

--- lib/src/core/constant/typography.dart
+++ lib/src/core/constant/typography.dart
@@ -23,30 +23,35 @@ const TextStyle h1 = TextStyle(
   fontSize: 24,
   fontWeight: FontWeight.w500,
   fontFamily: fontFamilyJostMedium,
+  package: "pdf_report_scope",
 );
 
 const TextStyle h2 = TextStyle(
   fontSize: 22,
   fontWeight: FontWeight.w500,
   fontFamily: fontFamilyJostMedium,
+  package: "pdf_report_scope",
 );
 
 const TextStyle h3 = TextStyle(
   fontSize: 20,
   fontWeight: FontWeight.w500,
   fontFamily: fontFamilyJostMedium,
+  package: "pdf_report_scope",
 );
 
 const TextStyle h4 = TextStyle(
   fontSize: 18,
   fontWeight: FontWeight.w500,
   fontFamily: fontFamilyJostMedium,
+  package: "pdf_report_scope",
 );
 
 const TextStyle h5 = TextStyle(
   fontSize: 16,
   fontWeight: FontWeight.w500,
   fontFamily: fontFamilyJostMedium,
+  package: "pdf_report_scope",
 );
 
 //Body Medium
@@ -54,30 +59,35 @@ const TextStyle b1Medium = TextStyle(
   fontSize: 18,
   fontWeight: FontWeight.w500,
   fontFamily: fontFamilyJostMedium,
+  package: "pdf_report_scope",
 );
 
 const TextStyle b2Medium = TextStyle(
   fontSize: 16,
   fontWeight: FontWeight.w500,
   fontFamily: fontFamilyJostMedium,
+  package: "pdf_report_scope",
 );
 
 const TextStyle b3Medium = TextStyle(
   fontSize: 14,
   fontWeight: FontWeight.w500,
   fontFamily: fontFamilyJostMedium,
+  package: "pdf_report_scope",
 );
 
 const TextStyle b4Medium = TextStyle(
   fontSize: 12,
   fontWeight: FontWeight.w500,
   fontFamily: fontFamilyJostMedium,
+  package: "pdf_report_scope",
 );
 
 const TextStyle b5Medium = TextStyle(
   fontSize: 10,
   fontWeight: FontWeight.w500,
   fontFamily: fontFamilyJostMedium,
+  package: "pdf_report_scope",
 );
 
 //Body Regular
@@ -85,28 +95,33 @@ const TextStyle b1Regular = TextStyle(
   fontSize: 18,
   fontWeight: FontWeight.w400,
   fontFamily: fontFamilyJostRegular,
+  package: "pdf_report_scope",
 );
 
 const TextStyle b2Regular = TextStyle(
   fontSize: 16,
   fontWeight: FontWeight.w400,
   fontFamily: fontFamilyJostRegular,
+  package: "pdf_report_scope",
 );
 
 const TextStyle b3Regular = TextStyle(
   fontSize: 14,
   fontWeight: FontWeight.w400,
   fontFamily: fontFamilyJostRegular,
+  package: "pdf_report_scope",
 );
 
 const TextStyle b4Regular = TextStyle(
   fontSize: 12,
   fontWeight: FontWeight.w400,
   fontFamily: fontFamilyJostRegular,
+  package: "pdf_report_scope",
 );
 
 const TextStyle b5Regular = TextStyle(
   fontSize: 10,
   fontWeight: FontWeight.w400,
   fontFamily: fontFamilyJostRegular,
+  package: "pdf_report_scope",
 );
