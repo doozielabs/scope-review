a1a21a2068db17f6f84b544b914443e70d9bfbf6
SyedMehtabali <syedmehtab2125@gmail.com>
2023-04-10 17:17:27 +0500

    Images testing 005

--- lib/src/utils/helpers/helper.dart
+++ lib/src/utils/helpers/helper.dart
@@ -149,11 +149,11 @@ extension StringExtension on String? {
   String get punctuation => set.isEmpty ? "" : ', $this';
   bool get isUrl => set.contains("http");
   bool get isDeviceUrl {
-    // if (Platform.isIOS) {
-    return set.contains("/var/mobile/Containers"); // for Physical Device
-    // return set.contains("data/Containers/"); // for Simulator
-    // }
-    // return set.contains("app.com.scope");
+    if (Platform.isIOS) {
+      return set.contains("/var/mobile/Containers"); // for Physical Device
+      // return set.contains("data/Containers/"); // for Simulator
+    }
+    return set.contains("app.com.scope");
   }
 
   bool get isAsset => set.contains("assets");
