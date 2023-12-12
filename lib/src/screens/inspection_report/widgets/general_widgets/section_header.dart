204b3aecab27b0f56a23150900dfb179ea89782f
MV Dev <mvdev@doozielabs.com>
2023-12-12 17:22:14 +0500

    Added new files for sections header and footer

---
+++ lib/src/screens/inspection_report/widgets/general_widgets/section_header.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pdf_report_scope/src/core/constant/colors.dart';
+import 'package:pdf_report_scope/src/core/constant/typography.dart';
+import 'package:pdf_report_scope/src/utils/helpers/helper.dart';
+
+class SectionHeader extends StatelessWidget {
+  final String title;
+  final String value;
+  const SectionHeader({
+    Key? key,
+    required this.title,
+    required this.value,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const SizedBox(height: 5),
+        Text(
+                value,
+                style: primaryHeadingTextStyle.copyWith(
+                    fontFamily: fontFamilyJostLight,
+                    fontWeight: FontWeight.w400,
+                    color: ProjectColors.firefly,
+                    fontSize: 14),
+                    textAlign:TextAlign.center,
+              ), 
+        const SizedBox(height: 10)           
+      ],
+    );
+  }
+}
