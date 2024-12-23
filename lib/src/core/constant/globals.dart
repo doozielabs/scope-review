262680b207ecf19d36c9e469ed3fc70691f2724f
Arsalan Baig <arsalan.baig@doozielabs.com>
2024-12-23 13:14:49 +0500

    Prod URL Update

--- lib/src/core/constant/globals.dart
+++ lib/src/core/constant/globals.dart
@@ -6,7 +6,7 @@ import 'package:pdf_report_scope/src/data/models/enum_types.dart';
 import 'package:pdf_report_scope/src/data/models/template_section.dart';
 import 'package:sizer/sizer.dart';
 
-const baseUrl = 'https://api.scopeinspectapp.com'; // Production
+const baseUrl = 'https://apiv1.scopeinspectapp.com'; // Production
 // const baseUrl = 'https://staging-api.scopeinspectapp.com'; // Staging
 // const baseUrl = 'http://localhost:1337'; // Local
 // const baseUrl = 'http://192.168.88.15:1337';
